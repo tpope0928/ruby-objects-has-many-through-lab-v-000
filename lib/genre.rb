0f410ce783330e9c4afb1f80dfa326062388a51a
Thomas Pope <tpope0928@gmail.com>
2018-09-10 13:29:11 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -5,7 +5,17 @@ class Genre
     @name = name
   end
   
-  
+  def songs
+    Song.all{|song| song.genre == self}
+  end
+
+  def artists
+    songs.map{|song| song.artist}
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
 
 
 end
